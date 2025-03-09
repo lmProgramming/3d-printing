13c57f31ea6cf0f999f9fdb02c0461bbc625a132
jantar <mikikubs@gmail.com>
2025-03-09 14:02:53 +0100

    feat: added typing

--- kitchen_fix/stl.py
+++ kitchen_fix/stl.py
@@ -5,12 +5,12 @@ outer_depth = 17.0
 outer_height = 22.0
 wall_thickness = 0.3
 
-left_wall = trimesh.creation.box(
+left_wall: trimesh.Trimesh = trimesh.creation.box(
     extents=[wall_thickness, outer_depth, outer_height])
-right_wall = trimesh.creation.box(
+right_wall: trimesh.Trimesh = trimesh.creation.box(
     extents=[wall_thickness, outer_depth, outer_height])
 
-front_wall = trimesh.creation.box(
+front_wall: trimesh.Trimesh = trimesh.creation.box(
     extents=[outer_width, wall_thickness, outer_height])
 
 left_wall.apply_translation(
@@ -20,7 +20,8 @@ right_wall.apply_translation(
 front_wall.apply_translation(
     [0, -outer_depth / 2 + wall_thickness / 2, outer_height / 2])
 
-stl = trimesh.util.concatenate([left_wall, right_wall, front_wall])
+stl: trimesh.Trimesh = trimesh.util.concatenate(
+    [left_wall, right_wall, front_wall])
 
 filename = "kitchen_fix/sliding_cover.stl"
 stl.export(filename)
